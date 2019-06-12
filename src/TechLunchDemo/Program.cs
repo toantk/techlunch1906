943fea85ff242ae9331b136c25f35da1bf36d971
Toan Truong <khaitoan@gmail.com>
2019-06-13 05:56:58 +0700

    #1 Add feature 1 implementation

--- src/TechLunchDemo/Program.cs
+++ src/TechLunchDemo/Program.cs
@@ -7,6 +7,7 @@ namespace TechLunchDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Work from Feature 1");
         }
     }
 }
