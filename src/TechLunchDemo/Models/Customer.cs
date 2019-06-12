e9859d208253ea16a3c39a3680ebc264b69baeda
Toan Truong <khaitoan@gmail.com>
2019-06-13 05:59:12 +0700

    #1 Add model and adjust Feature 1

---
+++ src/TechLunchDemo/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechLunchDemo.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
